bb3a001e07909c5a0174b62b67567feaf5f84d35
Ryan Daley <rpdaley@gmail.com>
2017-03-15 13:46:47 -0400

    express_server

---
+++ express_server.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+
+
+var PORT = process.env.PORT || 8080; // default port 8080
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  var shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  //console.log(urlDatabase)
+  delete urlDatabase[req.params.id];
+  //console.log(urlDatabase)
+  res.redirect('/urls');
+});
+
+app.post("/urls/:id", (req, res) => {
+  console.log(urlDatabase)
+  urlDatabase[req.params.id] = req.body.longURL;
+  console.log(urlDatabase)
+  res.redirect('/urls/');
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  var myShortURL = req.params.shortURL;
+  if (myShortURL in urlDatabase){
+    let longURL = urlDatabase[req.params.shortURL];
+    if (!(longURL.startsWith('http://') || longURL.startsWith('https://'))){
+      longURL = 'http://' + longURL;
+    }
+    res.redirect(301, longURL);
+  } else {
+    res.redirect(404, "404");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/404", (req, res) => {
+  res.render("404");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6)
+}
